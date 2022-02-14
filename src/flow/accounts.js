42f5aca54151aa421c0a8e462124762f88d59f91
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2022-02-14 14:15:54 -0800

    Adds FLIP 200 mechanics to derive all accounts controlled by a ledger device

--- src/flow/accounts.js
+++ src/flow/accounts.js
@@ -1,6 +1,12 @@
 import * as fcl from "@onflow/fcl"
 import { config } from "@onflow/fcl"
 
+import { 
+  getPublicKey as getPublicKeyOnDevice,
+  getPath,
+  LEGACY_PATH_ADDRESS,
+} from "../ledger/ledger.js"
+
 const accountsPath = "/accounts"
 
 const signatureAlgorithm = "ECDSA_P256";
@@ -64,3 +70,135 @@ export const getKeyIdForKeyByAccountAddress = async (address, publicKey) => {
 
   return key.index
 };
+
+
+//
+
+export const getNextAvailableAccountPath = async (accounts) => {
+  accounts = accounts.filter(account => !account.isLegacyAccount)
+
+  let sortedAccountsByAccountIndex = accounts.sort((a, b) => a.accountIndex - b.accountIndex)
+
+  let minUnusedAccountIndex = sortedAccountsByAccountIndex[0].accountIndex
+  sortedAccountsByAccountIndex.forEach(account => {
+    if (minUnusedAccountIndex + 1 === account.accountIndex) minUnusedAccountIndex = account.accountIndex
+  })
+  let nextAvailableAccountIndex = minUnusedAccountIndex
+  
+  let accountsAtNextAvailableAccountIndex = accounts.map(account => account.accountIndex = nextAvailableAccountIndex) 
+  let minUnusedKeyIndex = 0
+  accountsAtNextAvailableAccountIndex.forEach(account => {
+    if (minUnusedKeyIndex + 1 === account.keyIndex) minUnusedKeyIndex = account.keyIndex
+  })
+  let nextAvailableAccountKeyIndex = minUnusedKeyIndex
+
+  return getPath(nextAvailableAccountIndex, nextAvailableAccountKeyIndex)
+}
+
+export const getNextAvailableAccountKeyPath = async (accounts, accountIndex) => {
+  accounts = accounts.filter(account => !account.isLegacyAccount)
+
+  let accountsAtAccountIndex = accounts.map(account => account.accountIndex = accountIndex) 
+  let minUnusedKeyIndex = 0
+  accountsAtAccountIndex.forEach(account => {
+    if (minUnusedKeyIndex + 1 === account.keyIndex) minUnusedKeyIndex = account.keyIndex
+  })
+  let nextAvailableAccountKeyIndex = minUnusedKeyIndex
+
+  return getPath(accountIndex, nextAvailableAccountKeyIndex)
+}
+
+export const getLegacyAddressAndPublicKey = async () => {
+  return await getAddressAndPublicKeyByPath(LEGACY_PATH_ADDRESS)
+}
+
+export const getAddressAndPublicKeyByPath = async (path) => {
+  const publicKey = await getPublicKeyOnDevice(path)
+  console.log("getAddressAndPublicKeyByPath path=", path, " publicKey=", publicKey)
+  let address = null
+  if (publicKey) address = await getAccount(publicKey)
+
+  return {
+    address,
+    publicKey,
+  }
+}
+
+export const getAllAddressAndPublicKeysByPaths = async () => {
+  let legacyAddressPublicKey = null
+  try {
+    legacyAddressPublicKey = await getLegacyAddressAndPublicKey()
+  } catch (e) {
+    console.log("getAllAddressAndPublicKeysByPaths: NO lEGACY ACCOUNT")
+  }
+
+  const MAX_ACCOUNT_GAP = 5
+  let currentAccountGap = 0
+  const MAX_KEY_GAP = 5
+  let currentKeyGap = 0
+
+  let accountIndex = 0
+  let keyIndex = 0
+
+  let accounts = {}
+
+  if (legacyAddressPublicKey && legacyAddressPublicKey.address !== null) {
+    accounts["legacy"] = [{
+      address: legacyAddressPublicKey.address,
+      publicKey: legacyAddressPublicKey.publicKey,
+      accountIndex: null,
+      keyIndex: null,
+      path: LEGACY_PATH_ADDRESS,
+      isLegacyAccount: true,
+    }]
+  }
+
+  while (currentAccountGap < MAX_ACCOUNT_GAP) {
+    const currentPath = getPath(accountIndex, keyIndex)
+
+    let currentAddressPublicKey = null
+    try {
+      currentAddressPublicKey = await getAddressAndPublicKeyByPath(currentPath)
+    } catch (e) {
+      console.log("getAddressAndPublicKeyByPath: NO ACCOUNT FOUND path=", currentPath)
+    }
+
+    if (currentAddressPublicKey.address !== null) {
+      currentAccountGap = 0
+      currentKeyGap = 0
+
+      let foundAccount = {
+        address: currentAddressPublicKey.address,
+        publicKey: currentAddressPublicKey.publicKey,
+        accountIndex,
+        keyIndex,
+        path: currentPath,
+        isLegacyAccount: false,
+      }
+
+      accounts[accountIndex] = accounts[accountIndex] 
+        ? accounts[accountIndex].push(foundAccount)
+        : [foundAccount]
+
+      keyIndex = keyIndex + 1
+
+    } else {
+      currentKeyGap = currentKeyGap + 1
+      keyIndex = keyIndex + 1
+
+      if (currentKeyGap >= MAX_KEY_GAP) {
+        if (!(accounts[accountIndex])) {
+          currentAccountGap = currentAccountGap + 1
+        }
+        accountIndex = accountIndex + 1
+
+        keyIndex = 0
+        currentKeyGap = 0
+      }
+    }
+  }
+
+  return Object.values(accounts).reduce((acc, curr) => {
+    acc.join(curr)
+  }, [])
+}
